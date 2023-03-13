19457104bc903613e0d4c89510ffd3077df687d7
ameza <ameza@AUSTIN>
2023-03-13 11:14:17 -0600

    Agregar archivos de proyecto.

---
+++ Models/Tarea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_Dev_JR.Models;
+
+public partial class Tarea
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public DateTime FechaTareaRegistro { get; set; }
+
+    public bool TareaCompletada { get; set; }
+
+    public DateTime? FechaTareaCompletada { get; set; }
+}
